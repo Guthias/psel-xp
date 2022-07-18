1008dc3a9972519689daae5c44c5a6ff043473c4
Gustavo Mathias <gmgustavom@gmail.com>
2022-07-18 01:00:12 -0300

    refac: formating stocks function
    writing function for format stocks from rawStocks.map outside function for after can use it again

--- src/services/StockService.ts
+++ src/services/StockService.ts
@@ -2,6 +2,23 @@ import BuyOrder from '../database/models/BuyOrderModel';
 import SellOrder from '../database/models/SellOrderModel';
 import Stocks from '../database/models/StockModel';
 import IRawStocks from '../interfaces/StocksInterface';
+import { ErrorsList } from '../middlewares/ErrorsMiddleware';
+
+const formatStock = ({
+  id, name, buyOrders, sellOrders,
+}: IRawStocks) => {
+  const numberBuyOrders = buyOrders.map(({ price }) => Number(price));
+  const numberSellOrders = sellOrders.map(({ price }) => Number(price));
+
+  return {
+    id,
+    name,
+    minBuyOrder: Math.min(...numberBuyOrders),
+    maxBuyOrder: Math.max(...numberBuyOrders),
+    minSellOrder: Math.min(...numberSellOrders),
+    maxSellOrder: Math.max(...numberSellOrders),
+  };
+};
 
 const getAllStocks = async () => {
   const rawStockList = await Stocks.findAll({
@@ -14,21 +31,7 @@ const getAllStocks = async () => {
     }],
   }) as unknown as IRawStocks[];
 
-  const stockList = rawStockList.map(({
-    id, name, buyOrders, sellOrders,
-  }) => {
-    const numberBuyOrders = buyOrders.map(({ price }) => Number(price));
-    const numberSellOrders = sellOrders.map(({ price }) => Number(price));
-
-    return {
-      id,
-      name,
-      minBuyOrder: Math.min(...numberBuyOrders),
-      maxBuyOrder: Math.max(...numberBuyOrders),
-      minSellOrder: Math.min(...numberSellOrders),
-      maxSellOrder: Math.max(...numberSellOrders),
-    };
-  });
+  const stockList = rawStockList.map(formatStock);
 
   return stockList;
 };
