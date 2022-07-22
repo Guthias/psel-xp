3bb2571406fe3014e1bf778706b7d30ec3646c84
Gustavo Mathias <gmgustavom@gmail.com>
2022-07-22 13:30:09 -0300

    feat(wip): completing buy orders
    creating transaction for decrement bought quantity from buy order and add stocks on user wallet
    
    wip: still needing add balance on seller account and decrease stocks from sell order

--- src/services/ExchangeService.ts
+++ src/services/ExchangeService.ts
@@ -5,6 +5,42 @@ import User from '../database/models/UserModel';
 import { ErrorsList } from '../middlewares/ErrorsMiddleware';
 import { IUserBalance } from '../interfaces/UserInterface';
 import IOrder from '../interfaces/IOrder';
+import Wallet from '../database/models/WalletModel';
+
+const updateBuyOrder = async (
+  { stockId, userId, quantity }: IOrder,
+  boughtQuantity: number,
+) => {
+  const t = await Sequelize.transaction();
+  try {
+    await BuyOrder.decrement(
+      { quantity: boughtQuantity },
+      { where: { stockId, userId }, transaction: t },
+    );
+    const { quantity: buyOrderQuantity } = BuyOrder.findOne(
+      { attributes: ['quantity'], where: { stockId, userId }, transaction: t },
+    ) as unknown as IOrder;
+
+    if (buyOrderQuantity === 0) {
+      await BuyOrder.destroy({ where: { stockId, userId }, transaction: t });
+    }
+
+    const buyerHasStock = await Wallet.findOne({ where: { stockId, userId } });
+
+    if (buyerHasStock) {
+      await Wallet.increment(
+        { quantity: boughtQuantity },
+        { where: { stockId, userId }, transaction: t },
+      );
+    } else {
+      await Wallet.create({ stockId, userId, quantity }, { transaction: t });
+    }
+    await t.commit();
+  } catch (e) {
+    await t.rollback();
+    throw e;
+  }
+};
 
 const makeExchanges = async (stockId: string, price: number) => {
   const highestBuyOrder = await BuyOrder.findOne({ where: { stockId, price } });
@@ -13,29 +49,33 @@ const makeExchanges = async (stockId: string, price: number) => {
   const lowestSellOrder = await SellOrder.findOne({ where: { stockId, price } });
   lowestSellOrder as unknown as IOrder;
 
+  // console.log('-------------');
+  // console.log(highestBuyOrder, lowestSellOrder);
+  // console.log('-------------');
   if (!highestBuyOrder || !lowestSellOrder) return;
 
-  const t = await Sequelize.transaction();
-  try {
-    if (highestBuyOrder.quantity > lowestSellOrder.quantity) {
-      // Adiciona ou cria ação na carteira
-      // Adiciona saldo no vendedor
-      // Remove ordem de compra
-      // Subtrai quantidade na ordem de venda
-    } else if (highestBuyOrder.quantity === lowestSellOrder.quantity) {
-      // Adiciona ou cria ação na carteira
-      // Adiciona saldo no vendedor
-      // Remove ordem de compra
-      // Remove ordem de venda
-    } else {
-      // Adiciona ou cria ação na carteira
-      // Adiciona saldo no vendedor
-      // Subtrai quantidade na ordem de compra
-      // Remove ordem de venda
-      makeExchanges(stockId, price);
-    }
-  } catch (e) {
-    t.rollback();
+  console.log('hb', highestBuyOrder.quantity);
+  console.log('ls', lowestSellOrder.quantity);
+
+  if (highestBuyOrder.quantity < lowestSellOrder.quantity) {
+    await updateBuyOrder(highestBuyOrder, highestBuyOrder.quantity);
+    // Adiciona saldo no vendedor
+    // Subtrai quantidade na ordem de venda
+    return;
+  } if (highestBuyOrder.quantity === lowestSellOrder.quantity) {
+    // Adiciona ou cria ação na carteira
+    // Remove ordem de compra
+
+    // Adiciona saldo no vendedor
+    // Remove ordem de venda
+  } else {
+    // Adiciona ou cria ação na carteira
+    // Subtrai quantidade na ordem de compra
+
+    // Adiciona saldo no vendedor
+    // Remove ordem de venda
+    console.log('caiu no else');
+    // makeExchanges(stockId, price);
   }
 };
 
@@ -80,7 +120,7 @@ const buyStocks = async (userId: number, stockId: string, quantity: number) => {
 
   const createdBuyOrder = await createBuyOrder(userId, stockId, marketPrice, quantity);
 
-  await makeExchanges(stockId, quantity);
+  await makeExchanges(stockId, marketPrice);
 
   return { marketPrice, createdBuyOrder };
 };
