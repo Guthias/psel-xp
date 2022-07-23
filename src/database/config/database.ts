8c0325b5ebb9219e8f5721d2ff01b1311a1791f8
Gustavo Mathias <gmgustavom@gmail.com>
2022-07-23 06:37:29 -0300

    chore: test environment
    installing jest, supertest and creating initial configs for test environment, like test database and tests folder
    
    I needed install cross-env for be easily use environment variables on windows

--- src/database/config/database.ts
+++ src/database/config/database.ts
@@ -1,10 +1,12 @@
 import 'dotenv/config';
 import { Options } from 'sequelize';
 
+const environment = process.env.NODE_ENV === 'test' ? '-test' : '';
+
 const config: Options = {
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
+  database: process.env.DB_DATABASE + environment,
   host: process.env.DB_HOST,
   port: Number(process.env.DB_PORT),
   dialect: process.env.DB_DIALECT as any,
