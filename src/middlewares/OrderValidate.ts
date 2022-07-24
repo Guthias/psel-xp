60a004bb3068e18b83f527fdec17ed3a25fb16e1
Gustavo Mathias <gmgustavom@gmail.com>
2022-07-24 01:30:38 -0300

    feat: middleware for verify order format
    creating middleware for verify if order request (buy or sell) have correct format

---
+++ src/middlewares/OrderValidate.ts
@@ -0,0 +1,25 @@
+import { NextFunction, Request, Response } from 'express';
+import Joi from 'joi';
+import { StatusCodes } from 'http-status-codes';
+import { CustomError } from './ErrorsMiddleware';
+
+const OrderSchema = Joi.object({
+  stockId: Joi.string().required(),
+  quantity: Joi.number().min(1).required(),
+});
+
+const OrderValidate = async (req: Request, res: Response, next: NextFunction) => {
+  const { error } = OrderSchema.validate(req.body);
+
+  if (error) {
+    const { type, message } = error.details[0];
+
+    if (type === 'any.required') throw CustomError(StatusCodes.BAD_REQUEST, message);
+
+    throw CustomError(StatusCodes.UNPROCESSABLE_ENTITY, message);
+  }
+
+  return next();
+};
+
+export default OrderValidate;
--- src/routes/exchange.routes.ts
+++ src/routes/exchange.routes.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
 import ExchangeController from '../controllers/ExchangeController';
 import AuthVerify from '../middlewares/AuthVerify';
+import OrderValidate from '../middlewares/OrderValidate';
 
 const routes = Router();
 
-routes.post('/buy', AuthVerify, ExchangeController.buyStocks);
+routes.post('/buy', AuthVerify, OrderValidate, ExchangeController.buyStocks);
 
 export default routes;
