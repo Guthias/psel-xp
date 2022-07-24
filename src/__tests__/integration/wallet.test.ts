319dbd9d12ffdd227f837cb9f17136bd7b1b8b73
Gustavo Mathias <gmgustavom@gmail.com>
2022-07-24 15:53:46 -0300

    test: creating tests for route <GET /wallet/buy>
    Creating test for check if route <GET /wallet/buy> is a authenticated protected route

--- src/__tests__/integration/wallet.test.ts
+++ src/__tests__/integration/wallet.test.ts
@@ -60,4 +60,23 @@ describe('/wallet', () => {
       });
     })
   });
+
+  describe('<GET /buy>', () => {
+    describe('It\'s an authorized route', () => {
+      it('Shouldn\'t be possible access without a token', async () => {
+        const result = await request(app).get('/wallet/buy');
+        
+        expect(result.status).toBe(401);
+        expect(result.body.message).toBe('Token not found');
+      })
+
+      it('Shouldn\'t be possible access with an invalid token', async () => {
+        const result = await request(app).get('/wallet/buy')
+          .set({ Authorization: 'tokenInvalido' });
+        
+        expect(result.status).toBe(401);
+        expect(result.body.message).toBe('Invalid or expired Token');
+      })
+    });
+  })
 })
