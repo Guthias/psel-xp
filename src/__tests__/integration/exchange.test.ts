4c5a537c67de2ddb884cae7fb515567fcd78fb27
Gustavo Mathias <gmgustavom@gmail.com>
2022-07-24 03:26:15 -0300

    test: creating tests for verify exchanges
    creating test for verify exchange behavior when bought quantity be smaller than available for sell quantity

--- src/__tests__/integration/exchange.test.ts
+++ src/__tests__/integration/exchange.test.ts
@@ -1,5 +1,12 @@
 import request from 'supertest';
 import app from '../../app';
+import SellOrder from '../../database/models/SellOrderModel';
+import Stocks from '../../database/models/StockModel';
+import Users from '../../database/models/UserModel';
+import Wallet from '../../database/models/WalletModel';
+import IOrder from '../../interfaces/IOrder';
+import { IUser } from '../../interfaces/UserInterface';
+import resetDatabase from './assets/resetDatabase';
 
 describe('/exchange' , () => {
   describe('<POST /buy>', () => {
@@ -108,6 +115,60 @@ describe('/exchange' , () => {
             expect(result.body.orderPrice).toBeDefined();
             expect(result.body.quantity).toBeDefined();
         });
+      })
+
+      describe('Verify if exchanges is made correctly', () => {  
+        describe('When bougth quantity be smaller than sell order', () => {
+          let buyOrderId:any;
+
+          beforeAll(async () => {
+            resetDatabase();
+
+            const { body:createdBuyOrder} = await request(app).post('/exchange/buy')
+              .set({ Authorization: token })
+              .send({
+                stockId: 'XPBR31',
+                quantity: 10,
+              });
+            
+              buyOrderId = createdBuyOrder.orderId
+          }, 60000);
+
+          it('Should decrease correctly user balance', async () => {
+            const { balance } = await Users
+              .findOne({ attributes: ['balance'], where: { id: 2 } }) as IUser;
+
+            expect(Number(balance)).toEqual(7019.2);
+          });
+
+          it('Should delete the buy order', async () => {
+            const buyOrder = await Users.findOne({ where: { id: buyOrderId }});
+
+            expect(buyOrder).toBeNull();
+          });
+
+          it('Should add stocks on buyer wallet', async () => {
+            const { quantity } = await Wallet.findOne({ where: {
+              stockId: 'XPBR31',
+              userId: 2,
+            }}) as unknown as { quantity: number}
+
+            expect(quantity).toBe(35)
+          });
+
+          it('Should decrease quantity from sell order', async () => {
+            const { quantity } = await SellOrder.findOne({ where: { id: 6 }}) as IOrder;
+
+            expect(quantity).toBe(10);
+          });
+
+          it('Should increase seller balance correctly', async () => {
+            const { balance } = await Users
+              .findOne({ attributes: ['balance'], where: { id: 4 } }) as IUser;
+
+            expect(Number(balance)).toEqual(7980.8);
+          });
+        })
       })
     });
   });
