faceda20ddf6e1809e6c0a0e8ae0ebf486af15d9
Gustavo Mathias <gmgustavom@gmail.com>
2022-07-24 01:25:06 -0300

    test: creating tests for <POST /exchange/buy>
    creating tests for verify if <POST /exchange/buy> can only be accessed with an valid token and if it validates request body format

---
+++ src/__tests__/integration/exchange.test.ts
@@ -0,0 +1,58 @@
+import request from 'supertest';
+import app from '../../app';
+
+describe('/exchange' , () => {
+  describe('<POST /buy>', () => {
+    let token: any;
+
+    beforeAll(async () => {
+      const response = await request(app).post('/login').send({
+        email: 'ada@teste.com',
+        password: '12345678'
+      });
+  
+      token = response.body.token;
+    });
+
+    describe('It\'s an authorized route', () => {
+      it('Shouldn\'t be possible access without a token', async () => {
+        const result = await request(app).post('/exchange/buy');
+        
+        expect(result.status).toBe(401);
+        expect(result.body.message).toBe('Token not found');
+      })
+
+      it('Shouldn\'t be possible access with an invalid token', async () => {
+        const result = await request(app).post('/exchange/buy')
+          .set({ Authorization: 'tokenInvalido' });
+        
+        expect(result.status).toBe(401);
+        expect(result.body.message).toBe('Invalid or expired Token');
+      })
+    });
+
+    describe('When invalid request body', () => {
+      it('Should have an error 400 when missing stockId', async () => {
+        const result = await request(app).post('/exchange/buy')
+          .set({ Authorization: token })
+          .send({
+            quantity: 50,
+          });
+        
+        expect(result.status).toBe(400);
+        expect(result.body.message).toBe('\"stockId\" is required');
+      })
+
+      it('Should have an error 400 when missing quantity', async () => {
+        const result = await request(app).post('/exchange/buy')
+          .set({ Authorization: token })
+          .send({
+            stockId: 'XPBR31',
+          });
+        
+        expect(result.status).toBe(400);
+        expect(result.body.message).toBe('\"quantity\" is required');
+      })
+    });
+  });
+});
