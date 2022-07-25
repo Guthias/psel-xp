8848047c8c7717afbfa481ff48f835d15915e4f3
Gustavo Mathias <gmgustavom@gmail.com>
2022-07-24 22:59:22 -0300

    build(chore): fixing seeds
    adding ids on seeds for be possible connect tables on external database

--- src/database/seeders/20220716224348-user.js
+++ src/database/seeders/20220716224348-user.js
@@ -5,21 +5,25 @@ module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.bulkInsert('users', [
       {
+        id: 1,
         email: 'admin@xpinc.com',
         name: 'Corretora XP Inc',
         password: bcrypt.hashSync('12345678', 10),
         balance: 1000000,
       }, {
+        id: 2,
         email: 'ada@teste.com',
         name: 'Ada Lovelace',
         password: bcrypt.hashSync('12345678', 10),
         balance: 8000,
       }, {
+        id: 3,
         email: 'alan@teste.com',
         name: 'Alan Turing',
         password: bcrypt.hashSync('12345678', 10),
         balance: 6800.23,
       }, {
+        id: 4,
         email: 'linus@teste.com',
         name: 'Linus Torvald',
         password: bcrypt.hashSync('12345678', 10),
